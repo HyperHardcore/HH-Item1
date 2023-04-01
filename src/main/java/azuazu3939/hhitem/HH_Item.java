3e37c2fdb3fc8fb7426c1d9bb6e6f49455255f46
azuazu3939 <azuazu3939@users.noreply.github.com>
2023-04-02 00:01:18 +0900

    Second

--- src/main/java/azuazu3939/hhitem/HH_Item.java
+++ src/main/java/azuazu3939/hhitem/HH_Item.java
@@ -4,8 +4,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HH_Item extends JavaPlugin {
 
+    private static HH_Item item;
+
     @Override
     public void onEnable() {
+        item = this;
         // Plugin startup logic
 
     }
@@ -14,4 +17,6 @@ public final class HH_Item extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public static HH_Item inst() {return item;}
 }
