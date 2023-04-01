c4916a4944d6457de9a9a339e2fbb2c3d511f860
azuazu3939 <azuazu3939@users.noreply.github.com>
2023-04-01 23:43:02 +0900

    First

---
+++ src/main/java/azuazu3939/hhitem/HH_Item.java
@@ -0,0 +1,17 @@
+package azuazu3939.hhitem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HH_Item extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
